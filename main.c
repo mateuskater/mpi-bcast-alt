c5a9d5c3f3aaebe91ce3002ab9dc0c22ec40c59b
MATEUS KATER POMBEIRO <mkp19@inf.ufpr.br>
2023-06-12 18:59:40 -0300

    adicionado chrono

--- main.c
+++ main.c
@@ -17,7 +17,6 @@
 #include "chrono.c"
 #include <assert.h>
 
-
 #define USE_MPI_Bcast 1  // do NOT change
 #define USE_my_Bcast 2   // do NOT change
 //choose either BCAST_TYPE in the defines bellow
@@ -46,13 +45,17 @@ const int SEED = 100;
         (( logic_rank + root ) % comm_size)        
 
 // int MPI_Bcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm)
-void my_Bcast_rb(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm)
-{
+void my_Bcast_rb(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm) {
 ////////////////// INSERIR AQUI SEU CODIGO
+    // int rank;
+    // MPI_Init();
+    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // if (rank == 0){
+    //     MPI_Send();
+    // }else{
 
-MPI_Init();
-
-MPI_Finalize();
+    // }
+    // MPI_Finalize();
 
 }
 
@@ -75,8 +78,47 @@ MPI_Finalize();
 //O tempo gasto nessa função de verificação 
 //  NAO deve influenciar suas medidas. 
 
+void verifica_my_Bcast_rb( void *buffer, int count, MPI_Datatype datatype,
+                           int root, MPI_Comm comm ){
+    int comm_size;
+    int my_rank;
+    
+    MPI_Comm_size( comm, &comm_size );
+    MPI_Comm_rank( comm, &my_rank );
+    static long *buff = (long *) calloc( count*comm_size, sizeof(long) );
+    
+    
+    // preenche a faixa do raiz com alguma coisa (apenas no raiz)
+    if( my_rank == root )
+       for( int i=0; i<count; i++ )
+          buff[ i ] = i+SEED;
+    
+    #if BCAST_TYPE == USE_MPI_Bcast
+       MPI_Bcast( buff, count, datatype, root, comm );
+    #elif BCAST_TYPE == USE_my_Bcast
+       my_Bcast_rb( buff, count, datatype, root, comm );
+    #else
+       assert( BCAST_TYPE == USE_MPI_Bcast || BCAST_TYPE == USE_my_Bcast );
+    #endif   
+	   
+    
+    // cada nodo verifica se sua faixa recebeu adequadamente o conteudo
+    int ok=1;
+    int i;
+    for( i=0; i<count; i++ )
+       if( buff[ i ] != i+SEED ) {
+          ok = 0;
+          break;
+       }
+    // imprime na tela se OK ou nao
+    if( ok )
+        fprintf( stderr, "MY BCAST VERIF: node %d received ok\n", my_rank );
+    else
+        fprintf( stderr, "MY BCAST VERIF: node %d NOT ok! local position: %d contains %ld\n",
+                           my_rank, i, buff[i] );
 
-/// ... // Declaração da verifica_my_Bcast_rb( ... ) pode ir aqui!
+   free(buff);      
+}
 
 int main(int argc, char *argv[]){
 
